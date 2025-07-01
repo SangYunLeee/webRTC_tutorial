018a62cf5c7abe4269180fa020f2ec65ac6e69cd
2sangyun <sororiri@gmail.com>
2025-07-02 00:24:01 +0900

    fix: offer 에 tracks 가 추가되면서 m value 에 상세 정보가 추가되는 것 확인

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const myVideoContainer = document.getElementById("myVideoContainer");
 const myVideoElement = document.getElementById("myVideo");
 
 let pc = null;
+let offer = null;
+let stats = null;
 // define a click event listener to trigger a GUM (get user media) request
-myVideoContainer.addEventListener("click", () => {
+myVideoContainer.addEventListener("click", async () => {
     navigator.mediaDevices.getUserMedia({
         video: true,
     })
-        .then(stream => {
+        .then(async stream => {
             myVideoElement.srcObject = stream; // add stream to our <video> element
             pc = new RTCPeerConnection();
             pc.addEventListener("icegatheringstatechange", (e) => {
@@ -20,6 +22,14 @@ myVideoContainer.addEventListener("click", () => {
             pc.addEventListener("icecandidate", (e) => {
                 console.log("icecandidate", e);
             });
+            stream.getTracks().forEach(track => {
+                console.log("trackAdded: ", track);
+                pc.addTrack(track);
+            });
+            offer = await pc.createOffer();
+            console.log("offer", offer);
+            stats = await pc.getStats();
+            console.log("stats", stats);
         })
         .catch(err => {
             console.log("an error occurred trying to get user's video feed", err);
